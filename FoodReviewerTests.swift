37ea87137d44bc2e4646eb91ce6db31e398c482e
paramireze <paramireze@gmail.com>
2018-07-08 16:59:30 -0500

    added unit tests

--- FoodReviewerTests.swift
+++ FoodReviewerTests.swift
@@ -24,4 +24,20 @@ class FoodReviewerTests: XCTestCase {
         XCTAssertNotNil(positiveRatingMeal)
     }
     
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+    
 }
