35ec7e45e6dfa5958c2e689e34e33379ea1b4c0c
paramireze <paramireze@gmail.com>
2018-07-08 20:38:42 -0500

    added table view cell

---
+++ FoodReviewer/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodReviewer
+//
+//  Created by Ramirez Paul E on 7/8/18.
+//  Copyright © 2018 Ramirez Paul E. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
